be5ff9ecd60c82501ca3e66a4df6edd1a56f5326
nnkfernandes <annykfsouza@gmail.com>
2023-06-30 19:15:17 -0300

    sei la

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,7 +3,9 @@ import { env } from '$env/dynamic/private';
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = () => {
+  console.log(env);
   const mapbox_key = env.MAPBOX_APIKEY;
+  console.log(mapbox_key)
   if (mapbox_key) {
     return {
       mapbox_key
