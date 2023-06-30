f8b20b5529cc5569a5350c810e4ad9abf073111c
davi-i <josedavifrancelino@gmail.com>
2023-06-30 12:36:46 -0300

    feat: added map

---
+++ src/lib/blocks/Map.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import mapboxgl, { Map } from 'mapbox-gl';
+	import { onMount } from 'svelte';
+
+	export let mapbox_key: string;
+	export let lat: number;
+	export let long: number;
+	export let zoom = 12;
+
+	onMount(() => {
+		mapboxgl.accessToken = mapbox_key;
+		const map = new Map({
+			container: 'map',
+			style: 'mapbox://styles/mapbox/streets-v11',
+			center: [lat, long],
+			zoom
+		});
+	});
+</script>
+
+<svelte:head>
+	<link href="https://api.mapbox.com/mapbox-gl-js/v2.15.0/mapbox-gl.css" rel="stylesheet" />
+</svelte:head>
+
+<div id="map" style="height: 400px;" />
