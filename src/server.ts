c2cdf7eec2252eca502b26edafccd876f86a4db3
davi-i <josedavifrancelino@gmail.com>
2023-07-01 00:53:09 -0300

    feat: added notifications

--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,24 @@ app.post('/api/sensorchange', (req, res) => {
   console.log(JSON.stringify(req.body));
   for (const response of req.body.contextResponses) {
     const element = response.contextElement;
+    const capacity = +element.attributes.filter(({ name }) => name == 'capacidade')[0].value;
+    const volume = +element.attributes.filter(({ name }) => name == 'volume')[0].value;
+    const vazao = +element.attributes.filter(({ name }) => name == 'vazao')[0].value;
+    const percent = volume / capacity;
+
+    if (percent > 30) {
+      element.situacao = 'Estável';
+    } else if (percent > 0) {
+      element.situacao = 'Crítica';
+      io.emit('critical');
+    } else {
+      element.situacao = 'Totalmente vazio';
+      io.emit('empty');
+    }
+
+    if (vazao >= 0.1 * capacity) {
+      io.emit('leaking');
+    }
     io.emit('sensorchange', element);
   }
   res.status(200);
